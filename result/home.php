9c4c80fc7fb833471b2d44dbc4c320427692f751
Sandhya <144054124+sandhya-1234@users.noreply.github.com>
2024-02-17 00:42:07 +0530

    Add files via upload

---
+++ result/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="home.css">
+<style>
+
+</style>
+
+</head>
+<body>
+
+<h1><font size="10">Result Management System</font></h1>
+
+<p><font size="5"><b>Select an option from below</b></font</p>
+
+<p><a href="./analysis/analysiss.php" class="button button1">Analysis</a></p>
+<p><a href="./students/students.php" class="button button1">Students</a></p>
+<p><a href="./department/departments.php" class="button button1">Departments</a></p>
+<p><a href="./ia/internal_marks.php" class="button button1">Internal Marks</a></p>
+<p><a href="./ea/external_marks.php" class="button button1">External Marks</a></p>
+<p><a href="./subjects/subjects.php" class="button button1">Subjects</a></p>
+
+</body>
+</html>
